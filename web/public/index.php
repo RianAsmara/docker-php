8f238ad605aa63c21b064684dd865dff301ea025
Rian Asmara Putra <gagahkalem@gmail.com>
2020-07-21 01:33:57 +0800

    Add Login PDO

--- web/public/index.php
+++ web/public/index.php
@@ -1,28 +1,44 @@
 <html>
-<head><title>PDO MySQL</title></head>
-	<style>
-	html, body {
-		margin: 1px;
-		border: 0;
-	}
-	</style>
+
+<head>
+  <title>Docker PHP Mysql PDO</title>
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+  integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<style>
+  html,
+  body {
+    margin: 1px;
+    border: 0;
+  }
+</style>
+
 <body>
-	<div align="center">
-		<div style=" border: solid 1px #079B96; " align="left">
-			<?php if (isset($errMsg)) {
-       echo '<div style="color:#FF0000;text-align:center;font-size:18px;">' .
-           $errMsg .
-           '</div>';
-   } ?>
-			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>PDO MySQL</b></div>
-			<div style="margin: 15px">
-				Welcome guest !
-				<br>
-				<a href="login.php">Login</a> <br>
-				<a href="register.php">Register</a> <br>
-				<a href="forgot.php">Forgot Password</a>
-			</div>
-		</div>
-	</div>
+  <div align="center">
+    <div style=" border: solid 1px #079B96; " align="left">
+      <?php if (isset($errMsg)) {
+          echo '<div style="color:#FF0000;text-align:center;font-size:18px;">' .
+              $errMsg .
+              '</div>';
+      } ?>
+      <div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Docker PHP Mysql</b></div>
+      <div style="margin: 15px">
+        Welcome guest !
+        <br>
+        <a href="login.php">Login</a> <br>
+        <a href="register.php">Register</a> <br>
+        <a href="forgot.php">Forgot Password</a>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
 </body>
+
 </html>
--- web/public/login.php
+++ web/public/login.php
@@ -1,75 +1,103 @@
 <?php
-	require 'config.php';
+require 'config.php';
 
-	if(isset($_POST['login'])) {
-		$errMsg = '';
+if (isset($_POST['login'])) {
+    $errMsg = '';
 
-		// Get data from FORM
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+    // Get data from FORM
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-		if($username == '')
-			$errMsg = 'Enter username';
-		if($password == '')
-			$errMsg = 'Enter password';
+    if ($username == '') {
+        $errMsg = 'Enter username';
+    }
+    if ($password == '') {
+        $errMsg = 'Enter password';
+    }
 
-		if($errMsg == '') {
-			try {
-				$stmt = $connect->prepare('SELECT id, fullname, username, password, secretpin FROM pdo WHERE username = :username');
-				$stmt->execute(array(
-					':username' => $username
-					));
-				$data = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($errMsg == '') {
+        try {
+            $stmt = $connect->prepare(
+                'SELECT id, fullname, username, password, secretpin FROM pdo WHERE username = :username'
+            );
+            $stmt->execute([
+                ':username' => $username,
+            ]);
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-				if($data == false){
-					$errMsg = "User $username not found.";
-				}
-				else {
-					if($password == $data['password']) {
-						$_SESSION['name'] = $data['fullname'];
-						$_SESSION['username'] = $data['username'];
-						$_SESSION['password'] = $data['password'];
-						$_SESSION['secretpin'] = $data['secretpin'];
+            if ($data == false) {
+                $errMsg = "User $username not found.";
+            } else {
+                if ($password == $data['password']) {
+                    $_SESSION['name'] = $data['fullname'];
+                    $_SESSION['username'] = $data['username'];
+                    $_SESSION['password'] = $data['password'];
+                    $_SESSION['secretpin'] = $data['secretpin'];
 
-						header('Location: dashboard.php');
-						exit;
-					}
-					else
-						$errMsg = 'Password not match.';
-				}
-			}
-			catch(PDOException $e) {
-				$errMsg = $e->getMessage();
-			}
-		}
-	}
+                    header('Location: dashboard.php');
+                    exit();
+                } else {
+                    $errMsg = 'Password not match.';
+                }
+            }
+        } catch (PDOException $e) {
+            $errMsg = $e->getMessage();
+        }
+    }
+}
 ?>
 
 <html>
-<head><title>Login</title></head>
-	<style>
-	html, body {
+
+<head>
+	<title>Login</title>
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+	integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+	integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+</script>
+<style>
+	html,
+	body {
 		margin: 1px;
 		border: 0;
 	}
-	</style>
+</style>
+
 <body>
-	<div align="center">
-		<div style=" border: solid 1px #006D9C; " align="left">
-			<?php
-				if(isset($errMsg)){
-					echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
-				}
-			?>
-			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Login</b></div>
-			<div style="margin: 15px">
-				<form action="" method="post">
-					<input type="text" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username'] ?>" autocomplete="off" class="box"/><br /><br />
-					<input type="password" name="password" value="<?php if(isset($_POST['password'])) echo $_POST['password'] ?>" autocomplete="off" class="box" /><br/><br />
-					<input type="submit" name='login' value="Login" class='submit'/><br />
-				</form>
+	<div class="container">
+		<div class="row justify-content-center mt-5">
+			<div style=" border: solid 1px #006D9C; ">
+				<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Login</b></div>
+				<div style="margin: 15px">
+					<form action="" method="post">
+						<input type="text" name="username" value="<?php if (isset($_POST['username'])) {
+          echo $_POST['username'];
+      } ?>" autocomplete="off" class="box" /><br /><br />
+						<input type="password" name="password" value="<?php if (
+          isset($_POST['password'])
+      ) {
+          echo $_POST['password'];
+      } ?>" autocomplete="off" class="box" /><br /><br />
+						<input type="submit" name='login' value="Login" class='submit btn btn-primary btn-block' />
+					</form>
+				</div>
+				<?php if (isset($errMsg)) {
+        echo '<div style="color:#FF0000;text-align:center;font-size:17px;">' .
+            $errMsg .
+            '</div>';
+    } ?>
+				<br>
 			</div>
 		</div>
 	</div>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+		integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+	</script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+		integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+	</script>
 </body>
+
 </html>
